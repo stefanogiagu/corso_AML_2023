780441b0ae90b69199287427143e64fb64117389
Andrea Ciardiello <andrea.ciardiello@gmail.com>
2023-03-08 15:57:53 +0100

    Update data_preprocess.py

--- notebooks/es1/data_preprocess.py
+++ notebooks/es1/data_preprocess.py
@@ -1,3 +1,22 @@
+/*
+* <This code is a part of the NA62 collaboration>
+* Copyright (C) <2019-2023>  <Ape group INFN roma + Andrea>
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
 from apeml import getData	# Data Preprocess
 from apeml import EventPlot	# Data Preprocess, closer to getData during development, to be moved in ape62
 from apeml import EventPrint  	# Data Preprocess, closer to getData during development, to be moved in ape62
